7a2b91151e79185f943541fd7af8b9ce30131099
Aaron <aaron@amorgan.net.nz>
2015-09-20 17:06:12 +1200

    Added UnitTest project.

---
+++ NancyDemo1/UnitTests/Of.Modules/Of.Api/SiteModuleFixture.cs
@@ -0,0 +1,38 @@
+﻿using Nancy;
+using Nancy.Testing;
+using NancyDemo1.Modules.Api;
+using NUnit.Framework;
+
+namespace UnitTests.Of.Modules.Of.Api
+{
+    [TestFixture]
+    public class SiteModuleFixture
+    {
+        Browser _browser;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetup()
+        {
+            _browser = new Browser(cfg =>
+            {
+                cfg.Module<SiteModule>();
+            });
+        }
+
+        [TestCase("/")]
+        public void Should_return_status_ok_when_route_exists(string route)
+        {
+            var result = SendRequest(route);
+
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        private BrowserResponse SendRequest(string route)
+        {
+            return _browser.Get(route, with =>
+            {
+                with.HttpRequest();
+            });
+        }
+    }
+}
