cadb54752e26a0e34f03c5ea6dff4dfeb8322cdd
Aaron <aaron@amorgan.net.nz>
2015-09-20 15:48:23 +1200

    Seed repository with a simple Nancy route.

---
+++ NancyDemo1/NancyDemo1/Modules/Api/SiteModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace NancyDemo1.Modules.Api
+{
+    public class SiteModule : NancyModule
+    {
+        public SiteModule()
+        {
+            Get["/"] = _ => "Hello from NancyDemo1";
+        }
+    }
+}
